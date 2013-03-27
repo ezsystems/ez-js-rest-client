66968339dea6346b25c10bd7928c9e0d8815bc38
Anton Petrushenkov <anton.petrushenkov@ez.no>
2013-03-27 17:38:59 +0200

    jsRESTClient prototype: Simple interface for testing

--- jsRestClientBundle/Resources/public/js/testCAPI.js
+++ jsRestClientBundle/Resources/public/js/testCAPI.js
@@ -8,28 +8,64 @@ var jsCAPI = new CAPI(
         authMethod : "HTTPBasicAuth"
     },
     "XHR");
+
 var contentService = jsCAPI.getContentService();
 
+var clientOutput = document.getElementById('output');
 
-contentService.loadSections(function(data){
-    console.log(data);
-});
+// Load single section example
+var loadSectionAnchor = document.getElementById('load-section');
+var loadSectionLoader = document.getElementById('load-section-loader');
+loadSectionAnchor.onclick = function(e){
+
+    loadSectionLoader.style.display = 'block';
+    e.preventDefault();
+    contentService.loadSection(2, function(data){
+        clientOutput.innerHTML = data;
+        loadSectionLoader.style.display = 'none';
+    });
+
+};
 
 
-contentService.loadSection(2, function(data){
-    console.log(data);
-});
+// Load sections list example
+var loadSectionsAnchor = document.getElementById('load-sections');
+var loadSectionsLoader = document.getElementById('load-sections-loader');
+loadSectionsAnchor.onclick = function(e){
+
+    loadSectionsLoader.style.display = 'block';
+    e.preventDefault();
+    contentService.loadSections(function(data){
+        clientOutput.innerHTML = data;
+        loadSectionsLoader.style.display = 'none';
+    });
+
+};
 
 
-//var sectionInput = {
-//    SectionInput : {
-//        identifier : "testSection" + Math.random()*1000000,
-//        name : "Test Section"
-//    }
-//}
-//
-//// compatibility remark: JSON API is supported in all modern browsers (IE-wise since IE8)
-//contentService.createSection(JSON.stringify(sectionInput), function(data){
-//    console.log(data);
-//});
+// Create section example
+var createSectionAnchor = document.getElementById('create-section');
+var createSectionLoader = document.getElementById('create-section-loader');
+createSectionAnchor.onclick = function(e){
+
+    createSectionLoader.style.display = 'block';
+    e.preventDefault();
+    var sectionInput = {
+        SectionInput : {
+            identifier : "testSection" + Math.random()*1000000,
+            name : "Test Section"
+        }
+    }
+
+    // compatibility remark: JSON API is supported in all modern browsers (IE-wise since IE8)
+    contentService.createSection(JSON.stringify(sectionInput), function(data){
+        clientOutput.innerHTML = data;
+        createSectionLoader.style.display = 'none';
+    });
+
+
+
+};
+
+
 
