de8c7a6e440ca06ebf5648368e0eb32dcb690dec
Anton Petrushenkov <anton.petrushenkov@ez.no>
2013-07-05 13:01:24 +0300

    small fixes to CAPI main file discovered during unit testing

--- jsRestClientBundle/Resources/public/js/REST-client/CAPI.js
+++ jsRestClientBundle/Resources/public/js/REST-client/CAPI.js
@@ -7,11 +7,11 @@ var CAPI = (function() {
      * @constructor
      * @param endPointUrl {string} url to REST root
      * @param authenticationAgent {object} literal object used to maintain authentication to REST server
-     * @param connectionType {string} string related to one of the special connection objects used to implement exact technique ("XHR", "JSONP" etc.)
      */
     var CAPI = function (endPointUrl, authenticationAgent) {
 
         this._contentService = null;
+        this._contentTypeService = null;
         this._userService = null;
 
         authenticationAgent.CAPI = this;
@@ -20,20 +20,20 @@ var CAPI = (function() {
         // Array of connections, should be filled-in in preferred order
         //TODO: consider moving to some sort of configuration file...
         var connectionStack = [
-            {
-                connection: XmlHttpRequestConnection
-            },
-            {
-                connection: MicrosoftXmlHttpRequestConnection
-            }
-        ],
-        connectionFactory = new ConnectionFeatureFactory(connectionStack),
-        connectionManager = new ConnectionManager(endPointUrl, authenticationAgent, connectionFactory),
-        //TODO: move hardcoded rootPath to the same config file as above...
-        discoveryService = new DiscoveryService('/api/ezp/v2/', connectionManager);
+                {
+                    connection: XmlHttpRequestConnection
+                },
+                {
+                    connection: MicrosoftXmlHttpRequestConnection
+                }
+            ],
+            connectionFactory = new ConnectionFeatureFactory(connectionStack),
+            connectionManager = new ConnectionManager(endPointUrl, authenticationAgent, connectionFactory),
+            //TODO: move hardcoded rootPath to the same config file as above...
+            discoveryService = new DiscoveryService('/api/ezp/v2/', connectionManager);
 
-        //TODO: move logRequests to the same config file as above...
-        connectionManager.logRequests = true;
+            //TODO: move logRequests to the same config file as above...
+            connectionManager.logRequests = true;
 
         /**
          * Get instance of Content Service
