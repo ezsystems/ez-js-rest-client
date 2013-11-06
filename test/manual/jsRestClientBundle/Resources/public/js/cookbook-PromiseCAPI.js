3853169153dc78c913a263bb867ff6b64b711a26
Anton Petrushenkov <wiseman.std@gmail.com>
2013-11-06 17:39:21 +0200

    Fix EZP-21666: refactor PromiseCAPI and PromiseService classes smallish cookbook fixes

--- test/manual/jsRestClientBundle/Resources/public/js/cookbook-PromiseCAPI.js
+++ test/manual/jsRestClientBundle/Resources/public/js/cookbook-PromiseCAPI.js
@@ -1,14 +1,12 @@
-/* global CAPI, SessionAuthAgent, PromiseCAPI */
+/* global eZ */
 // Some simple PromiseCAPI usage scenario
 
 var jsCAPI = new eZ.CAPI(
         'http://ez.git.local',
         new eZ.SessionAuthAgent({login: "admin", password: "ezpublish"}),
-        {
-            logRequests: true
-        }
+        {logRequests: true}
     ),
-    jsPromiseCAPI = new eZ.PromiseCAPI(jsCAPI);
+    jsPromiseCAPI = new eZ.PromiseCAPI(jsCAPI),
     promiseContentService = jsPromiseCAPI.getContentService(),
     promise = promiseContentService.loadSection("/api/ezp/v2/content/sections/1");
 
