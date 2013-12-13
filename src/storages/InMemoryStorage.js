18dafc74be422497c3eda8e58363599ded43c91d
Jakob Westhoff <jakob@westhoffswelt.de>
2013-12-13 12:08:38 +0100

    Implemented and tested InMemoryStorage to be used for SinglePageApplications

---
+++ src/storages/InMemoryStorage.js
@@ -0,0 +1,78 @@
+/* global define */
+define(["structures/CAPIError"], function(CAPIError) {
+    /**
+     * Implementation of the storage abstraction utilizing simple in-memory variables
+     *
+     * This storage is always usable. Its main purpose is the usage in conjunction with
+     * Single-Page-Applications, which do not need fancy session or domain based storages,
+     * which are available for more than one request.
+     *
+     * @class InMemoryStorage
+     * @constructor
+     */
+    var InMemoryStorage = function() {
+        /**
+         * Storage object, which is used internally to store all the given data
+         *
+         * @property _storage
+         * @type {Object}
+         * @private
+         */
+        this._storage = {};
+    };
+
+    /**
+     * Retrieve an item from the storage
+     *
+     * @method getItem
+     * @param {string} key
+     * @return {*}
+     */
+    InMemoryStorage.prototype.getItem = function(key) {
+        if (this._storage[key] === undefined) {
+            return null;
+        }
+
+        // We store the items JSON encoded to not pass object and array references around
+        return JSON.parse(this._storage[key]);
+    };
+
+    /**
+     * Store an item in storage
+     *
+     * @method setItem
+     * @param {string} key
+     * @param {*} value
+     */
+    InMemoryStorage.prototype.setItem = function(key, value) {
+        // We store the items JSON encoded to not pass object and array references around
+        this._storage[key] = JSON.stringify(value);
+    };
+
+    /**
+     * Remove an item from storage
+     *
+     * @method removeItem
+     * @param {string} key
+     */
+    InMemoryStorage.prototype.removeItem = function(key) {
+        if (this._storage[key] !== undefined) {
+            delete this._storage[key];
+        }
+    };
+
+    /**
+     * Check whether this storage implementation is compatible with the current environment.
+     *
+     * The InMemoryStorage is always compatible with any environment.
+     *
+     * @method isComaptible
+     * @static
+     * @return {boolean}
+     */
+    InMemoryStorage.isCompatible = function() {
+        return true;
+    };
+
+    return InMemoryStorage;
+});
---
+++ test/InMemoryStorage.tests.js
@@ -0,0 +1,111 @@
+/* global define, describe, it, expect, beforeEach */
+define(["storages/InMemoryStorage"], function (InMemoryStorage) {
+    describe("InMemoryStorage", function() {
+        describe("Compatibility", function() {
+            it("should always be compatible", function() {
+                expect(InMemoryStorage.isCompatible()).toBeTruthy();
+            });
+        });
+
+        describe("Storage API", function() {
+            var storage,
+                storageData;
+
+            beforeEach(function() {
+                storage = new InMemoryStorage();
+
+                // This is kind of a hack, as we are accessing a private member variable,
+                // but it makes testing the basic functionality a lot easier.
+                storageData = storage._storage;
+
+            });
+
+            it("should store data under the given key", function() {
+                storage.setItem("someKey", "someValue");
+
+                // Double quoting is expected as everything is stored and encoded as json
+                expect(storageData.someKey).toBe('"someValue"');
+            });
+
+            it("should allow retrieval of data under a given key", function() {
+                storageData.someKey = '"someValue"';
+
+                expect(storage.getItem("someKey")).toBe("someValue");
+            });
+
+            it("should allow removal of data under a given key", function() {
+                storageData.someKey = '"someValue"';
+
+                storage.removeItem("someKey");
+
+                expect(storageData).toEqual({});
+            });
+
+            it("should overwrite data if key is already in use", function() {
+                storageData.someKey = '"someValue"';
+
+                storage.setItem("someKey", "someOtherValue");
+
+                // Double quoting is expected as everything is stored and encoded as json
+                expect(storageData.someKey).toBe('"someOtherValue"');
+            });
+
+            it("should return null if requested key does not exist", function() {
+                expect(storage.getItem("nonExistantKey")).toBeNull();
+            });
+
+            it("should do nothing if non existant key is removed", function() {
+                storage.removeItem("nonExistantKey");
+                // No exception, nothing ;)
+            });
+        });
+
+        describe("Arbitrary Data Storage", function() {
+            var storage;
+
+            function storeAndRetrieve(value) {
+                var result;
+                storage.setItem("myGreatKey", value);
+                result = storage.getItem("myGreatKey");
+
+                expect(result).toEqual(value);
+                expect(typeof result).toEqual(typeof value);
+            }
+
+            beforeEach(function() {
+                storage = new InMemoryStorage();
+            });
+
+            it("should store and retrieve strings", function() {
+                storeAndRetrieve("some String");
+            });
+
+            it("should store and retrieve integers", function() {
+                storeAndRetrieve(23);
+            });
+
+            it("should store and retrieve floats", function() {
+                storeAndRetrieve(42.3);
+            });
+
+            it("should store and retrieve booleans", function() {
+                storeAndRetrieve(true);
+            });
+
+            it("should store and retrieve arrays", function() {
+                storeAndRetrieve([1,"two", "three", 4]);
+            });
+
+            it("should store and retrieve objects", function() {
+                storeAndRetrieve({
+                    some: {
+                        nicely: "nested",
+                        obj: ["ect", "structure"],
+                        or: "what?",
+                        1: 2
+                    }
+                });
+            });
+        });
+    });
+});
