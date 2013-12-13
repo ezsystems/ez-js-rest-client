cc6b15d43f59cea226370c88e33ff9efb378e5af
Jakob Westhoff <jakob@westhoffswelt.de>
2013-12-13 17:02:46 +0100

    Refactored both `Connections` to use prototypical inheritance instead of constructor inheritance

--- src/connections/MicrosoftXmlHttpRequestConnection.js
+++ src/connections/MicrosoftXmlHttpRequestConnection.js
@@ -12,58 +12,58 @@ define(["structures/Response", "structures/CAPIError"], function (Response, CAPI
      */
     var MicrosoftXmlHttpRequestConnection = function () {
         this._xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    };
 
-        /**
-         * Basic request implemented via XHR technique
-         *
-         * @method execute
-         * @param request {Request} structure containing all needed params and data
-         * @param callback {function} function, which will be executed on request success
-         */
-        this.execute = function (request, callback) {
-            var XHR = this._xhr,
-                headerType;
+    /**
+     * Basic request implemented via XHR technique
+     *
+     * @method execute
+     * @param request {Request} structure containing all needed params and data
+     * @param callback {function} function, which will be executed on request success
+     */
+    MicrosoftXmlHttpRequestConnection.prototype.execute = function (request, callback) {
+        var XHR = this._xhr,
+            headerType;
 
-            // Create the state change handler:
-            XHR.onreadystatechange = function () {
-                if (XHR.readyState != 4) {return;} // Not ready yet
-                if (XHR.status >= 400) {
-                    callback(
-                        new CAPIError("Connection error : " + XHR.status + ".", {
-                            errorCode : XHR.status,
-                            xhr: XHR
-                        }),
-                        false
-                    );
-                    return;
-                }
-                // Request successful
+        // Create the state change handler:
+        XHR.onreadystatechange = function () {
+            if (XHR.readyState != 4) {return;} // Not ready yet
+            if (XHR.status >= 400) {
                 callback(
-                    false,
-                    new Response({
-                        status: XHR.status,
-                        headers: XHR.getAllResponseHeaders(),
-                        body: XHR.responseText
-                    })
+                    new CAPIError("Connection error : " + XHR.status + ".", {
+                        errorCode : XHR.status,
+                        xhr: XHR
+                    }),
+                    false
                 );
-            };
-
-            if (request.httpBasicAuth) {
-                XHR.open(request.method, request.url, true, request.login, request.password);
-            } else {
-                XHR.open(request.method, request.url, true);
+                return;
             }
-
-            for (headerType in request.headers) {
-                if (request.headers.hasOwnProperty(headerType)) {
-                    XHR.setRequestHeader(
-                        headerType,
-                        request.headers[headerType]
-                    );
-                }
-            }
-            XHR.send(request.body);
+            // Request successful
+            callback(
+                false,
+                new Response({
+                    status: XHR.status,
+                    headers: XHR.getAllResponseHeaders(),
+                    body: XHR.responseText
+                })
+            );
         };
+
+        if (request.httpBasicAuth) {
+            XHR.open(request.method, request.url, true, request.login, request.password);
+        } else {
+            XHR.open(request.method, request.url, true);
+        }
+
+        for (headerType in request.headers) {
+            if (request.headers.hasOwnProperty(headerType)) {
+                XHR.setRequestHeader(
+                    headerType,
+                    request.headers[headerType]
+                );
+            }
+        }
+        XHR.send(request.body);
     };
 
     /**
@@ -78,5 +78,4 @@ define(["structures/Response", "structures/CAPIError"], function (Response, CAPI
     };
 
     return MicrosoftXmlHttpRequestConnection;
-
 });
--- src/connections/XmlHttpRequestConnection.js
+++ src/connections/XmlHttpRequestConnection.js
@@ -11,58 +11,58 @@ define(["structures/Response", "structures/CAPIError"], function (Response, CAPI
      */
     var XmlHttpRequestConnection = function () {
         this._xhr = new XMLHttpRequest();
+    };
 
-        /**
-         * Basic request implemented via XHR technique
-         *
-         * @method execute
-         * @param request {Request} structure containing all needed params and data
-         * @param callback {function} function, which will be executed on request success
-         */
-        this.execute = function (request, callback) {
-            var XHR = this._xhr,
-                headerType;
+    /**
+     * Basic request implemented via XHR technique
+     *
+     * @method execute
+     * @param request {Request} structure containing all needed params and data
+     * @param callback {function} function, which will be executed on request success
+     */
+    XmlHttpRequestConnection.prototype.execute = function (request, callback) {
+        var XHR = this._xhr,
+            headerType;
 
-            // Create the state change handler:
-            XHR.onreadystatechange = function () {
-                if (XHR.readyState != 4) {return;} // Not ready yet
-                if (XHR.status >= 400) {
-                    callback(
-                        new CAPIError("Connection error : " + XHR.status + ".", {
-                            errorCode : XHR.status,
-                            xhr: XHR
-                        }),
-                        false
-                    );
-                    return;
-                }
-                // Request successful
+        // Create the state change handler:
+        XHR.onreadystatechange = function () {
+            if (XHR.readyState != 4) {return;} // Not ready yet
+            if (XHR.status >= 400) {
                 callback(
-                    false,
-                    new Response({
-                        status: XHR.status,
-                        headers: XHR.getAllResponseHeaders(),
-                        body: XHR.responseText
-                    })
+                    new CAPIError("Connection error : " + XHR.status + ".", {
+                        errorCode : XHR.status,
+                        xhr: XHR
+                    }),
+                    false
                 );
-            };
-
-            if (request.httpBasicAuth) {
-                XHR.open(request.method, request.url, true, request.login, request.password);
-            } else {
-                XHR.open(request.method, request.url, true);
+                return;
             }
-
-            for (headerType in request.headers) {
-                if (request.headers.hasOwnProperty(headerType)) {
-                    XHR.setRequestHeader(
-                        headerType,
-                        request.headers[headerType]
-                    );
-                }
-            }
-            XHR.send(request.body);
+            // Request successful
+            callback(
+                false,
+                new Response({
+                    status: XHR.status,
+                    headers: XHR.getAllResponseHeaders(),
+                    body: XHR.responseText
+                })
+            );
         };
+
+        if (request.httpBasicAuth) {
+            XHR.open(request.method, request.url, true, request.login, request.password);
+        } else {
+            XHR.open(request.method, request.url, true);
+        }
+
+        for (headerType in request.headers) {
+            if (request.headers.hasOwnProperty(headerType)) {
+                XHR.setRequestHeader(
+                    headerType,
+                    request.headers[headerType]
+                );
+            }
+        }
+        XHR.send(request.body);
     };
 
     /**
@@ -77,5 +77,4 @@ define(["structures/Response", "structures/CAPIError"], function (Response, CAPI
     };
 
     return XmlHttpRequestConnection;
-
 });
