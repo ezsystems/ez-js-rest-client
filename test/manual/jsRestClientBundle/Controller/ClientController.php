9404f0848ceb28db6a590a69889d64cb93cf4057
André R <ar@ez.no>
2014-05-20 21:39:43 +0200

    Fix EZP-22128: Update copyright info on old headers

--- test/manual/jsRestClientBundle/Controller/ClientController.php
+++ test/manual/jsRestClientBundle/Controller/ClientController.php
@@ -4,7 +4,7 @@
  *
  * Simple prototype for content management via REST interface.
  *
- * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
