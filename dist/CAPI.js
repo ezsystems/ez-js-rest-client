2cd0fb8ff728620be23e023bef8776c641cd01ce
Damien Pobel <dp@ez.no>
2014-12-04 10:30:28 +0100

    Rebuild after EZP-22017 implementation

--- dist/CAPI.js
+++ dist/CAPI.js
@@ -4845,6 +4845,24 @@ define('services/ContentService',["structures/ContentCreateStruct", "structures/
         );
     };
 
+    /**
+     * Loads an image variation
+     *
+     * @method loadImageVariation
+     * @param variation {String} The variation REST id
+     * @param callback {Function} Callback executed after performing the request (see
+     *  {{#crossLink "ContentService"}}Note on the callbacks usage{{/crossLink}} for more info)
+     */
+    ContentService.prototype.loadImageVariation = function (variation, callback) {
+        this._connectionManager.request(
+            "GET",
+            variation,
+            "",
+            {"Accept": "application\/vnd.ez.api.ContentImageVariation+json"},
+            callback
+        );
+    };
+
     return ContentService;
 
 });
