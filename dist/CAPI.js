5446e593817fb31de0c0dea76875c49ceb5cfe84
Damien Pobel <dp@ez.no>
2015-11-17 09:45:25 +0100

    Rebuild CAPI

--- dist/CAPI.js
+++ dist/CAPI.js
@@ -1636,21 +1636,21 @@ define('structures/ContentCreateStruct',[],function () {
      * @param locationCreateStruct {LocationCreateStruct} create structure for a Location object, where the new Content object will be situated
      * @param languageCode {String} The language code (e.g. "eng-GB")
      */
-    var ContentCreateStruct = function (contentTypeId, locationCreateStruct, languageCode) {
+    var ContentCreateStruct = function (contentTypeId, locationCreateStruct, languageCode, alwaysAvailable) {
         var now = JSON.parse(JSON.stringify(new Date()));
 
         this.body = {};
         this.body.ContentCreate = {};
 
         this.body.ContentCreate.ContentType = {
-                "_href": contentTypeId
-            };
+            "_href": contentTypeId
+        };
 
         this.body.ContentCreate.mainLanguageCode = languageCode;
         this.body.ContentCreate.LocationCreate = locationCreateStruct.body.LocationCreate;
 
         this.body.ContentCreate.Section = null;
-        this.body.ContentCreate.alwaysAvailable = "true";
+        this.body.ContentCreate.alwaysAvailable = alwaysAvailable;
         this.body.ContentCreate.remoteId = null;
         this.body.ContentCreate.modificationDate = now;
         this.body.ContentCreate.fields = {};
@@ -3191,8 +3191,8 @@ define('services/ContentService',["structures/ContentCreateStruct", "structures/
      * @param language {String} The language code (eng-GB, fre-FR, ...)
      * @return {ContentCreateStruct}
      */
-    ContentService.prototype.newContentCreateStruct = function (contentTypeId, locationCreateStruct, language) {
-        return new ContentCreateStruct(contentTypeId, locationCreateStruct, language);
+    ContentService.prototype.newContentCreateStruct = function (contentTypeId, locationCreateStruct, language, alwaysAvailable) {
+        return new ContentCreateStruct(contentTypeId, locationCreateStruct, language, alwaysAvailable);
     };
 
     /**
