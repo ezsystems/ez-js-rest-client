93543cfd155de05a34f5c6fdbe5d72f8ee182374
Damien Pobel <dp@ez.no>
2015-12-10 08:35:35 +0100

    EZP-25258: Improved the script to generate the doc

--- bin/generate_apidoc.sh
+++ bin/generate_apidoc.sh
@@ -1,20 +1,26 @@
 #! /bin/sh
+# Script to generate the client API doc and to push the generated
+# to a repository in a given branch.
+# This script expects the $GH_TOKEN variable to be set to a valid
+# Github user token who is allowed to push on the target repository
 
 JS_REST_CLIENT_DIR="javascript-rest-client"
 TRIGGER_COMMIT_MSG=`git log --oneline -1`
-# TODO replace to ezsystems/ezsystems.github.com
+# TODO change to ezsystems/ezsystems.github.com
 DOC_REPOSITORY="dpobel/test-doc"
-# TODO replace to master
+# TODO change to master
 DOC_BRANCH=gh-pages
+GITHUB_USER="eZ Robot, I do what I'm told to..."
+# TODO change to ezrobot's email
+GITHUB_USER_EMAIL="dp@ez.no"
 
 grunt doc
 git clone https://${GH_TOKEN}@github.com/${DOC_REPOSITORY}.git ../ezsystems.github.com
 
 cd ../ezsystems.github.com
 
-git config user.name "eZ Robot, I do what I'm told to..."
-# TODO replace with ezrobot's email
-git config user.email "dp@ez.no"
+git config user.name "$GITHUB_USER"
+git config user.email "$GITHUB_USER_EMAIL"
 
 git checkout "$DOC_BRANCH"
 rsync -av --checksum $TRAVIS_BUILD_DIR/api/ "$JS_REST_CLIENT_DIR/"
