9da5ddc43dbbeaa341bc2474f9bd5ff3ba19527a
stephaneDiot <stephane.diot@ez.no>
2016-11-30 16:24:08 +0100

    Rebuild CAPI

--- dist/CAPI.js
+++ dist/CAPI.js
@@ -2060,6 +2060,14 @@ define('structures/ViewCreateStruct',[],function () {
             type = "ContentQuery";
         }
         /**
+         * Holds the view type 
+         *
+         * @property _type
+         * @protected
+         * @type {String}
+         */
+        this._type = type;
+        /**
          * Holds the body of the view create structs
          *
          * @property body
@@ -2093,6 +2101,93 @@ define('structures/ViewCreateStruct',[],function () {
             "Accept": "application/vnd.ez.api.View+json; version=1.1",
             "Content-Type": "application/vnd.ez.api.ViewInput+json; version=1.1"
         };
+    };
+
+    /**
+     * Gets the Criteria property
+     *
+     * @method getCriteria
+     * @return {Object} the criteria property.
+     * @deprecated
+     */
+    ViewCreateStruct.prototype.getCriteria = function () {
+        return this.body.ViewInput[this._type].Criteria;
+    };
+
+    /**
+     * Gets the Query property
+     *
+     * @method getQuery
+     * @return {Object} the query property.
+     */
+    ViewCreateStruct.prototype.getQuery = function () {
+        return this.body.ViewInput[this._type].Query;
+    };
+
+    /**
+     * Gets the Filter property
+     *
+     * @method getFilter
+     * @return {Object} the Filter property.
+     */
+    ViewCreateStruct.prototype.getFilter = function () {
+        return this.body.ViewInput[this._type].Filter;
+    };
+
+    /**
+     * Sets the Criteria property
+     *
+     * @method setCriteria
+     * @deprecated
+     */
+    ViewCreateStruct.prototype.setCriteria = function (criteria) {
+        this.body.ViewInput[this._type].Criteria = criteria;
+    };
+
+    /**
+     * Sets the Query property
+     *
+     * @method setQuery
+     */
+    ViewCreateStruct.prototype.setQuery = function (query) {
+        this.body.ViewInput[this._type].Query = query;
+    };
+
+    /**
+     * Sets the Filter property
+     *
+     * @method setFilter
+     */
+    ViewCreateStruct.prototype.setFilter = function (filter) {
+        this.body.ViewInput[this._type].Filter = filter;
+    };
+
+    /**
+     * Sets the SortClauses property
+     *
+     * @method setSortClauses
+     */
+    ViewCreateStruct.prototype.setSortClauses = function (sortClauses) {
+        this.body.ViewInput[this._type].SortClauses = sortClauses;
+    };
+
+    /**
+     * Sets the FacetBuilders property
+     *
+     * @method setFacetBuilders
+     */
+    ViewCreateStruct.prototype.setFacetBuilders = function (facetBuilders) {
+        this.body.ViewInput[this._type].FacetBuilders = facetBuilders;
+    };
+
+    /**
+     * Sets the limit and offset properties
+     *
+     * @method setLimitAndOffset
+     */
+    ViewCreateStruct.prototype.setLimitAndOffset = function (limit, offset) {
+        this.body.ViewInput[this._type].limit = limit;
+        this.body.ViewInput[this._type].offset = offset;
     };
 
     return ViewCreateStruct;
@@ -4205,8 +4300,8 @@ define('services/ContentService',["structures/ContentCreateStruct", "structures/
      * @param callback {Function} callback executed after performing the request (see
      *  {{#crossLink "ContentService"}}Note on the callbacks usage{{/crossLink}} for more info)
      * @example
-     *     var viewCreateStruct = contentService.newViewCreateStruct('some-test-id');
-     *     viewCreateStruct.body.ViewInput.Query.Criteria = {
+     *     var viewCreateStruct = contentService.newViewCreateStruct('some-test-id', 'LocationQuery');
+     *     viewCreateStruct.body.ViewInput.LocationQuery.Query = {
      *         FullTextCriterion : "title"
      *     };
      *     contentService.createView(
@@ -4224,7 +4319,10 @@ define('services/ContentService',["structures/ContentCreateStruct", "structures/
                     callback(error, views);
                     return;
                 }
-
+                if ( viewCreateStruct.getCriteria() && Object.keys(viewCreateStruct.getCriteria()).length !== 0 ) {
+                    console.warn('[DEPRECATED] virtual property Criteria is deprecated');
+                }
+                
                 that._connectionManager.request(
                     "POST",
                     views._href,
